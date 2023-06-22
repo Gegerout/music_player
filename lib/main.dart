887186ff319b5bd84a9ec5cf207087f1e39f60e1
Gegerout <selimchik06@gmail.com>
2023-06-22 19:24:54 +0300

    download songs update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:music_player/home/presentation/pages/home_page.dart';
 import 'package:music_player/home/presentation/pages/welcome_page.dart';
 
 void main() {
