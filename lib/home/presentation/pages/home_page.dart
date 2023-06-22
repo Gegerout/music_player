8472fff3b5c008061f141941a959562b2578c77e
Gegerout <selimchik06@gmail.com>
2023-06-22 15:55:41 +0300

    download songs update

--- lib/home/presentation/pages/home_page.dart
+++ lib/home/presentation/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:music_player/home/presentation/states/home_state.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +13,62 @@ class HomePage extends ConsumerStatefulWidget {
 class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset("assets/images/home_gradient.png"),
+          ref.watch(homeProvider).when(
+                data: (value) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 40),
+                        Container(
+                          width: double.infinity,
+                          height: 70,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.white.withOpacity(0.06)),
+                          child: Center(
+                            child: TextFormField(
+                              style: GoogleFonts.urbanist(
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 20,
+                                  color: Colors.white),
+                              decoration: InputDecoration(
+                                prefixIcon: Padding(
+                                  padding: const EdgeInsets.only(left: 25, right: 20),
+                                  child: Image.asset("assets/images/search_icon.png", width: 22, height: 22, fit: BoxFit.contain,),
+                                ),
+                                contentPadding: EdgeInsets.zero,
+                                hintText: "search song",
+                                hintStyle: GoogleFonts.urbanist(
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 20,
+                                    color: Colors.white.withOpacity(0.4)),
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                error: (error, stacktrace) {
+                  return Center(
+                    child: Text(error.toString()),
+                  );
+                },
+                loading: () => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+        ],
+      ),
+    );
   }
 }
--- lib/home/presentation/states/home_state.dart
+++ lib/home/presentation/states/home_state.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../data/repositories/data_repository.dart';
 
-final welcomeProvider = FutureProvider((ref) async {
+final homeProvider = FutureProvider((ref) async {
   final data = await DataRepository().getSongs();
   if(data != null) {
     return data.data;
