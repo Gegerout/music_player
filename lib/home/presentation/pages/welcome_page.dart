cf5a11a189f8bb4f7f5224ee50b819da05145b74
Gegerout <selimchik06@gmail.com>
2023-06-22 15:34:05 +0300

    download songs update

--- lib/home/presentation/pages/welcome_page.dart
+++ lib/home/presentation/pages/welcome_page.dart
@@ -1,33 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:music_player/home/presentation/states/welcome_state.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:music_player/home/presentation/pages/home_page.dart';
 
 class WelcomePage extends ConsumerWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref.watch(welcomeProvider).when(
-        data: (value) {
-          return Scaffold(
-            body: Column(
-              children: [
-                Text(value.data[0].title),
-              ],
+    return Scaffold(
+        body: Stack(
+      children: [
+        Image.asset("assets/images/bg_gradient.png"),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Image.asset(
+              "assets/images/headphones.png",
+              width: double.infinity,
+              height: 357,
+              fit: BoxFit.cover,
             ),
-          );
-        },
-        error: (error, stacktrace) {
-          return Scaffold(
-            body: Center(
-              child: Text(error.toString()),
+            Center(
+              child: Text("Getting Started",
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 38,
+                      color: Colors.white)),
             ),
-          );
-        },
-        loading: () => const Scaffold(
-          body: Center(
-                child: CircularProgressIndicator(),
-              ),
-        ));
+            Text("Getting started Getting",
+                style: GoogleFonts.urbanist(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8))),
+            const SizedBox(height: 50),
+            SizedBox(
+              width: 200,
+              height: 70,
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                        (route) => false);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF886AE2),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(180))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Let's go",
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                              color: Colors.white)),
+                      const SizedBox(
+                        width: 24,
+                      ),
+                      const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      )
+                    ],
+                  )),
+            ),
+            const Spacer(),
+            Text("DIEMU",
+                style: GoogleFonts.urbanist(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    color: Colors.white)),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Text("BEST MUSIC APP",
+                  style: GoogleFonts.urbanist(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 6,
+                      color: Colors.white.withOpacity(0.5))),
+            ),
+          ],
+        )
+      ],
+    ));
   }
 }
---
+++ lib/home/presentation/states/home_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../data/repositories/data_repository.dart';
+
+final welcomeProvider = FutureProvider((ref) async {
+  final data = await DataRepository().getSongs();
+  if(data != null) {
+    return data.data;
+  }
+  else {
+    throw Error();
+  }
+});
